b705c83ecf9a1f44c99cec6e0f239c66eb492cf4
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2024-07-07 00:57:32 +0300

    Pages changes

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ import Config
 
 # Configure your database
 config :discuss, Discuss.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "usgrant",
+  password: "",
   hostname: "localhost",
   database: "discuss_dev",
   stacktrace: true,
